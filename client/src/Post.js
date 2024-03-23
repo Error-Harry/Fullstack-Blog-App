94cde88c6577c3af9e7162ddf9bf5812449fe55e
Harsh Nargide <nargideharsh@gmail.com>
2024-03-24 00:57:23 +0530

    dynamic page contetn

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,24 +1,21 @@
 import React from "react";
 
-function Post() {
+function Post({title, summary, cover, content, createdAt}) {
   return (
     <div className="post">
       <div className="image">
         <img src="https://source.unsplash.com/random/?blog/" alt="" />
       </div>
       <div className="text">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+        <h2>{title}</h2>
         <p className="info">
           <a href="" className="author">
             Harsh Nargide
           </a>
-          <time>2024-03-14 00:27</time>
+          <time>{createdAt}</time>
         </p>
         <p className="summary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odit
-          saepe nisi officia voluptatibus quidem, facere, enim incidunt
-          perferendis eaque, laborum non veniam ipsum quis fugiat. Doloremque
-          ducimus ex molestias?
+          {summary}
         </p>
       </div>
     </div>
